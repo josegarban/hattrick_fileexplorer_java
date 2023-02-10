b8123aa128d0a66c859ebadf2d5c21aa27593f63
josegarban <josegarban@gmail.com>
2023-02-10 15:58:49 +0100

    First commit

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.hattrick_fileexplorer_java {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens com.example.hattrick_fileexplorer_java to javafx.fxml;
-    exports com.example.hattrick_fileexplorer_java;
-}
